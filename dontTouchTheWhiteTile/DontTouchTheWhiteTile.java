32be30e50414deed67b3a8d61b6ff86a7a8b6d90
ChunYaoChang <b07902022@csie.ntu.edu.tw>
2021-06-28 16:57:52 +0800

    update

--- dontTouchTheWhiteTile/DontTouchTheWhiteTile.java
+++ dontTouchTheWhiteTile/DontTouchTheWhiteTile.java
@@ -286,8 +286,8 @@ public class DontTouchTheWhiteTile implements ActionListener, MouseListener, Key
 				if (tile.pointInTile(x, y) && !tile.clicked) {
 					// System.out.println("fuck2 " + tile.x + " " + tile.y + " " + tile.black);
 					if (tile.black) {
-						score += Math.max(100 - milSecDelay, 10);
-						System.out.println("You've scored " + Math.max(100 - milSecDelay, 10) + " points!");
+						score += (100 + 10 * combo);
+						System.out.println("You've scored " + (100 + 10 * combo) + " points!");
 						milSecDelay = 0;
 						tile.clicked = true;
 						combo += 1;
